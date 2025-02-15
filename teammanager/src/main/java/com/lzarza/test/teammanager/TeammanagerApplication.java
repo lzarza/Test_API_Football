f844f02562431a1c3f1846f9687f2c39a12c7323
lzarza <laurentzarza@gmail.com>
2025-02-15 17:07:49 +0100

    Initial API import

---
+++ teammanager/src/main/java/com/lzarza/test/teammanager/TeammanagerApplication.java
@@ -0,0 +1,13 @@
+package com.lzarza.test.teammanager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeammanagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeammanagerApplication.class, args);
+	}
+
+}
---
+++ teammanager/src/test/java/com/lzarza/test/teammanager/TeammanagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lzarza.test.teammanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeammanagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
